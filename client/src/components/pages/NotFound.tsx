dcd3a8845bee954b75e30368418a64f8cae0c35c
Flash <flash.jaq@gmail.com>
2019-10-03 13:44:21 +0200

    Converted NotFound to typescript

--- client/src/components/pages/NotFound.tsx
+++ client/src/components/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 
-const NotFound = () => (
+const NotFound: React.FC = () => (
   <Fragment>
     <h1>Not Found</h1>
     <p className="lead">The page you are looking for does not exist.</p>
