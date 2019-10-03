acd1e14bddf76a628c5d05523c86aec0c45ce5b0
Flash <flash.jaq@gmail.com>
2019-10-03 13:43:21 +0200

    Converted types to typescript

---
+++ client/src/context/types.ts
@@ -0,0 +1,13 @@
+export const SET_ALERT: string = "SET_ALERT";
+export const REMOVE_ALERT: string = "REMOVE_ALERT";
+
+export const REGISTER_SUCCESS: string = "REGISTER_SUCCESS";
+export const REGISTER_FAIL: string = "REGISTER_FAIL";
+
+export const USER_LOADED: string = "USER_LOADED";
+export const AUTH_ERROR: string = "AUTH_ERROR";
+export const LOGIN_SUCCESS: string = "LOGIN_SUCCESS";
+export const LOGIN_FAIL: string = "LOGIN_FAIL";
+export const LOGOUT: string = "LOGOUT";
+
+export const CLEAR_ERRORS: string = "CLEAR_ERRORS";
