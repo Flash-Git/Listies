a788e5d70ae76136743c0ef0a6412d36241c6f79
Flash <flash.jaq@gmail.com>
2019-10-04 19:48:57 +0200

    Added container to alerts

--- client/src/components/layout/Alerts.tsx
+++ client/src/components/layout/Alerts.tsx
@@ -14,26 +14,28 @@ const Alerts: React.FC = () => {
   const { alerts, removeAlert } = alertContext;
 
   return (
-    alerts.length > 0 &&
-    alerts.map(({ id, type, msg }: IAlert) => (
-      <div key={id} className={`alert alert-${type}`}>
-        <div>
-          <FontAwesomeIcon icon={["fas", "info-circle"]} /> {msg}
-        </div>
-        <button
-          style={{
-            padding: "0.5rem 1rem",
-            margin: "-0.5rem",
-            border: "none",
-            background: "none"
-          }}
-          className={`alert-${type} btn`}
-          onClick={e => removeAlert(id)}
-        >
-          <FontAwesomeIcon icon={["fas", "times-circle"]} />
-        </button>
-      </div>
-    ))
+    <div className="container">
+      {alerts.length > 0 &&
+        alerts.map(({ id, type, msg }: IAlert) => (
+          <div key={id} className={`alert alert-${type}`}>
+            <div>
+              <FontAwesomeIcon icon={["fas", "info-circle"]} /> {msg}
+            </div>
+            <button
+              style={{
+                padding: "0.5rem 1rem",
+                margin: "-0.5rem",
+                border: "none",
+                background: "none"
+              }}
+              className={`alert-${type} btn`}
+              onClick={e => removeAlert(id)}
+            >
+              <FontAwesomeIcon icon={["fas", "times-circle"]} />
+            </button>
+          </div>
+        ))}
+    </div>
   );
 };
 
