cd8fd2b8dd9d1e7e579746d7fab962017cbcfd17
Flash <flash.jaq@gmail.com>
2019-10-04 19:49:41 +0200

    Added container

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,14 +1,19 @@
-import React, { Fragment, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Lists from "../lists/Lists";
 import ListForm from "../lists/ListForm";
 import Items from "../items/Items";
+import ItemForm from "../items/ItemForm";
 
 import AuthContext from "../../context/auth/AuthContext";
+import ListContext from "../../context/list/ListContext";
 
 const Home: React.FC = () => {
   const authContext = useContext(AuthContext);
   const { loadUser } = authContext;
+
+  const listContext = useContext(ListContext);
+  const { currentList } = listContext;
 
   const [loaded, setLoaded] = useState(false);
 
@@ -20,17 +25,28 @@ const Home: React.FC = () => {
       loadUser();
     }, []);
   };
-
   useLoad();
 
+  const listId = currentList ? currentList.id : "";
+  const listName = currentList ? currentList.name : "No List Selected";
+
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
+        marginTop: "0.7rem"
+      }}
+    >
+      <div style={{ justifySelf: "flex-start" }}>
         <ListForm />
         <Lists />
       </div>
-      <div>
-        <Items />
+      <div style={{ margin: "0 auto", padding: "0 2rem" }}>
+        <ItemForm listId={listId} listName={listName} />
+        <Items listId={listId} listName={listName} />
       </div>
     </div>
   );
