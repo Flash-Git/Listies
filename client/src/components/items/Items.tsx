37e3df1b90e4829d9ed43082f798eb27d94cb94c
Flash <flash.jaq@gmail.com>
2019-10-04 19:49:14 +0200

    Added items component

---
+++ client/src/components/items/Items.tsx
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect, Fragment } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import PropTypes from "prop-types";
+
+import Item from "./Item";
+import Spinner from "../layout/Spinner";
+
+import ItemContext from "../../context/item/ItemContext";
+
+import { IItem } from "../../context/item/IItem";
+
+const Items: any = ({ listId }: any) => {
+  const itemContext = useContext(ItemContext);
+
+  const { loading, items, getItems } = itemContext;
+
+  useEffect(() => {
+    if (listId === "") return;
+    getItems(listId);
+    //eslint-disable-next-line
+  }, [listId]);
+
+  if (items && items.length === 0 && !loading) {
+    return <></>;
+  }
+
+  return (
+    <Fragment>
+      {items && !loading ? (
+        <TransitionGroup>
+          {items.map((item: IItem) => (
+            <CSSTransition key={item.id} timeout={200} classNames="item">
+              <Item item={item} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  );
+};
+
+Item.propTypes = {
+  listId: PropTypes.object
+};
+
+export default Items;
