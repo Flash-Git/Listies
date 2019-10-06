d308ea9b3d95cee01a122b6c745b616d137186df
Flash <flash.jaq@gmail.com>
2019-10-06 18:50:19 +0200

    Logging

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+console.log(process.env.MONGO_URI);
+
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const connectDB = require("./config/db");
@@ -17,8 +19,6 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/lists", require("./routes/api/lists"));
 app.use("/api/items", require("./routes/api/items"));
-
-console.log(process.env.MONGO_URI);
 
 //Serve static assets in production
 if (process.env.NODE_ENV === "production") {
