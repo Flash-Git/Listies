bbf21f379b0522771e8081fce51f986ce4b418de
Flash <flash.jaq@gmail.com>
2019-10-06 18:27:36 +0200

    Added clg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-require("dotenv").config();
+const conf = require("dotenv").config();
+console.log(conf);
 
 const connectDB = require("./config/db");
 
