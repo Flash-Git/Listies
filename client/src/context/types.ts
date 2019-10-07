4c798345823713effeb9c91a9349c6d62c31324a
Flash <flash.jaq@gmail.com>
2019-10-07 17:09:26 +0200

    Renamed GET_LIST to GET_LISTS

--- client/src/context/types.ts
+++ client/src/context/types.ts
@@ -15,7 +15,7 @@ export const LOGOUT: string = "LOGOUT";
 export const CLEAR_ERRORS: string = "CLEAR_ERRORS";
 
 // List
-export const GET_LIST: string = "GET_LIST";
+export const GET_LISTS: string = "GET_LISTS";
 export const ADD_LIST: string = "ADD_LIST";
 export const SET_CURRENT: string = "SET_CURRENT";
 export const CLEAR_CURRENT: string = "CLEAR_CURRENT";
