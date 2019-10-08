6b7f54090f0f8c6201fa35a4b55a8f63a70b860d
Flash <flash.jaq@gmail.com>
2019-10-09 00:17:17 +0200

    Added drag and drop ordering

--- client/src/components/items/Items.tsx
+++ client/src/components/items/Items.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, Fragment } from "react";
+import React, { useContext, useEffect, useState, Fragment } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import PropTypes from "prop-types";
 
@@ -12,7 +12,7 @@ import { IItem } from "../../context/item/IItem";
 const Items: any = ({ listId }: any) => {
   const itemContext = useContext(ItemContext);
 
-  const { loading, items, getItems } = itemContext;
+  const { loading, items, getItems, setItems } = itemContext;
 
   useEffect(() => {
     if (listId === "") return;
@@ -20,13 +20,57 @@ const Items: any = ({ listId }: any) => {
     //eslint-disable-next-line
   }, [listId]);
 
+  const initialState: IItem | any = null;
+  const [draggedItem, setDraggedItem] = useState(initialState);
+
+  const onDragStart = (e: any, index: number) => {
+    setDraggedItem(items[index]);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+  };
+
+  const onDragOver = (index: number) => {
+    const draggedOverItem = items[index];
+
+    // if the item is dragged over itself, ignore
+    if (draggedItem === null || draggedItem.id === draggedOverItem.id) return;
+
+    // filter out the currently dragged item
+    let otherItems = items.filter(
+      (item: IItem) => draggedItem && item.id !== draggedItem.id
+    );
+
+    // add the dragged item after the dragged over item
+    otherItems.splice(index, 0, draggedItem);
+
+    setItems(otherItems);
+  };
+
+  const onDragEnd = () => {
+    setDraggedItem(null);
+  };
+
   return (
     <Fragment>
       {items && !loading ? (
         <TransitionGroup>
-          {items.map((item: IItem) => (
-            <CSSTransition key={item.id} timeout={200}>
-              <Item item={item} />
+          {items.map((item: IItem, i: number) => (
+            <CSSTransition
+              key={item.id}
+              timeout={200}
+              onDragOver={() => onDragOver(i)}
+            >
+              <div>
+                <div
+                  className="drag"
+                  draggable
+                  onDragStart={e => onDragStart(e, i)}
+                  onDragEnd={onDragEnd}
+                >
+                  <Item item={item} />
+                </div>
+              </div>
             </CSSTransition>
           ))}
         </TransitionGroup>
--- client/src/components/lists/Lists.tsx
+++ client/src/components/lists/Lists.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, Fragment } from "react";
+import React, { useContext, useEffect, useState, Fragment } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import ListContext from "../../context/list/ListContext";
@@ -10,20 +10,64 @@ import { IList } from "../../context/list/IList";
 const Lists = () => {
   const listContext = useContext(ListContext);
 
-  const { loading, lists, getLists } = listContext;
+  const { loading, lists, getLists, setLists } = listContext;
 
   useEffect(() => {
     getLists();
     //eslint-disable-next-line
   }, []);
 
+  const initialState: IList | any = null;
+  const [draggedList, setDraggedList] = useState(initialState);
+
+  const onDragStart = (e: any, index: number) => {
+    setDraggedList(lists[index]);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+  };
+
+  const onDragOver = (index: number) => {
+    const draggedOverItem = lists[index];
+
+    // if the item is dragged over itself, ignore
+    if (draggedList === null || draggedList.id === draggedOverItem.id) return;
+
+    // filter out the currently dragged item
+    let otherLists = lists.filter(
+      (list: IList) => draggedList && list.id !== draggedList.id
+    );
+
+    // add the dragged item after the dragged over item
+    otherLists.splice(index, 0, draggedList);
+
+    setLists(otherLists);
+  };
+
+  const onDragEnd = () => {
+    setDraggedList(null);
+  };
+
   return (
     <Fragment>
       {lists && !loading ? (
         <TransitionGroup>
-          {lists.map((list: IList) => (
-            <CSSTransition key={list.id} timeout={200}>
-              <ListItem list={list} />
+          {lists.map((list: IList, i: number) => (
+            <CSSTransition
+              key={list.id}
+              timeout={200}
+              onDragOver={() => onDragOver(i)}
+            >
+              <div>
+                <div
+                  className="drag"
+                  draggable
+                  onDragStart={e => onDragStart(e, i)}
+                  onDragEnd={onDragEnd}
+                >
+                  <ListItem list={list} />
+                </div>
+              </div>
             </CSSTransition>
           ))}
         </TransitionGroup>
