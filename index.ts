5562f6a2a8fe76f634b14f9dc8737ee2a6c16b57
J Quinn <git@jaquinn.com>
2021-03-16 03:59:05 +0000

    Renamed server to index

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express, { Express } from "express";
 
 import Server from "./server/server";
 
-const app = express();
+const app: Express = express();
 
 const PORT = process.env.PORT || 5000;
 
-const server = new Server(app);
+const server: Server = new Server(app);
 
 server.start(PORT);
