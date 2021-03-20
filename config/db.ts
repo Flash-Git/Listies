1a4fe7844f4a0d7745ae299179f4f48fe7e73bfa
J Quinn <git@jaquinn.com>
2021-03-20 23:40:00 +0000

    Formatting

--- config/db.ts
+++ config/db.ts
@@ -3,9 +3,7 @@ import config from "config";
 
 const connectDB = async () => {
   const db: string =
-    process.env.NODE_ENV === "production"
-      ? process.env.MONGO_URI
-      : config.get("mongoURI");
+    process.env.NODE_ENV === "production" ? process.env.MONGO_URI : config.get("mongoURI");
 
   try {
     await mongoose.connect(db, {
