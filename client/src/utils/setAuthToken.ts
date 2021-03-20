a9c2f975e3370e3ddeaeaaadc4d7375547e4ca50
J Quinn <git@jaquinn.com>
2021-03-20 19:34:13 +0000

    Update authToken variable name

--- client/src/utils/setAuthToken.ts
+++ client/src/utils/setAuthToken.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const setAuthToken = (token: string) => {
-  if (token) {
-    axios.defaults.headers.common["x-auth-token"] = token;
-  } else {
-    delete axios.defaults.headers.common["x-auth-token"];
-  }
+const updateAuthTokenHeader = (token?: string) => {
+  if (token) axios.defaults.headers.common["x-auth-token"] = token;
+  else delete axios.defaults.headers.common["x-auth-token"];
 };
 
-export default setAuthToken;
+export default updateAuthTokenHeader;
