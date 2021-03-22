addb6302e6a9b772213a7253b72b214c281173aa
J Quinn <git@jaquinn.com>
2021-03-22 11:47:44 +0000

    Cleaned up ItemForm and state

---
+++ client/src/components/layout/Sorter.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { SortItems } from "context";
+
+interface Props {
+  sortItems: SortItems;
+}
+
+const Sorter: FC<Props> = ({ sortItems }) => (
+  <button
+    className="btn btn-link"
+    style={{
+      float: "right",
+      marginRight: "1rem",
+      height: "2.4rem",
+    }}
+    onClick={sortItems}
+  >
+    <FontAwesomeIcon style={{ height: "100%" }} icon={["fas", "sort-up"]} />
+  </button>
+);
+
+export default Sorter;
