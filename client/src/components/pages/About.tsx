f021db183c71b1a520f848d4c97654727591cbcd
J Quinn <git@jaquinn.com>
2021-04-13 22:11:14 +0100

    Version bump

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -19,7 +19,7 @@ const About: FC = () => {
       <h1>About</h1>
       <p className="my-1">Full stack React app</p>
       <p className="bg-dark p">
-        <strong>Version: </strong> 1.0.0
+        <strong>Version: </strong> 1.1.0
       </p>
     </div>
   );
