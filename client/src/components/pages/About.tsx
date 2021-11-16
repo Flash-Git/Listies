b84396e883e2c78f9625c882f063a6fbb7cee5e6
J Quinn <git@jaquinn.com>
2021-11-16 19:15:54 +0000

    Removed weird setLoading code

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -1,19 +1,6 @@
-import { FC, useContext } from "react";
-
-import { useMountEffect } from "../../functions/hooks";
-
-import AuthContext from "../../context/auth/AuthContext";
-
-import { AuthContext as IAuthContext } from "context";
+import { FC } from "react";
 
 const About: FC = () => {
-  const authContext: IAuthContext = useContext(AuthContext);
-  const { setLoading } = authContext;
-
-  useMountEffect(() => {
-    setLoading(true);
-  });
-
   return (
     <div className="container">
       <h1>About</h1>
