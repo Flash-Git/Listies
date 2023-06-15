b39f58785567395fb2524595d910b9f6074184a5
Quinn <git@jaquinn.com>
2023-06-15 04:32:20 +0100

    Added modal component

---
+++ client/src/components/layout/Modal.tsx
@@ -0,0 +1,66 @@
+import { FC, useState } from "react";
+
+interface Props {
+  action(): void;
+  undoAction?(): void;
+  title: string;
+  content: string;
+  closeModal(): void;
+}
+
+const Modal: FC<Props> = (props) => {
+  const [hovering, setHovering] = useState(true);
+
+  return (
+    <div
+      className="dialog-container dialog-overlay"
+      onClick={() => {
+        !hovering && props.closeModal();
+      }}>
+      <div
+        className="card bg-light my dialog-content"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "0.7rem",
+          boxShadow: "0 0 2px 0px rgba(0,0,0,0.35)",
+        }}
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        title={props.title}>
+        <p className="dialog-content text-primary text-left grow-shrink">{props.content}</p>
+        <div
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}>
+          <button
+            className="btn-discreet"
+            style={{
+              margin: "0.5rem",
+            }}
+            onClick={() => {
+              props.action();
+              props.closeModal();
+            }}>
+            Confirm
+          </button>
+          <button
+            className="btn-discreet"
+            style={{
+              margin: "0.5rem",
+            }}
+            onClick={() => {
+              props.undoAction && props.undoAction();
+              props.closeModal();
+            }}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
